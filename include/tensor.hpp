e692d0e9bd1cf2ac7c9454e39c86b5abae0909e4
killf <killf@foxmail.com>
2019-09-19 19:28:22 +0800

    代码迁移

--- include/tensor.hpp
+++ include/tensor.hpp
@@ -2,50 +2,174 @@
 #define TENSORSCRIPT_TENSOR_HPP
 
 #include <memory>
+#include <vector>
+#include <sstream>
+#include <assert.h>
 
 namespace TensorScript {
-    using namespace std;
+		using namespace std;
 
-    /**
-     * 数据类型
-     * */
-    enum DataType {
-        i8, i16, i32, i64, u8, u16, u32, u64, f16, f32, f64
-    };
+		/**
+		 * 值错误
+		 * */
+		class ValueError : public exception {
+		public:
+				explicit ValueError(const string &&msg) : _msg(msg) {}
 
-    /**
-     * 张量的形状
-     * */
-    class TensorShape {
+				const char *what() const noexcept override {
+					return _msg.c_str();
+				}
 
-    };
+		private:
+				string _msg;
+		};
 
-    /**
-     * 张量
-     * */
-    class Tensor {
-    public:
-        Tensor(TensorShape shape, DataType dataType);
+		/**
+		 * 数据类型
+		 * */
+		enum DataType {
+				i8, i16, i32, i64, u8, u16, u32, u64, f16, f32, f64
+		};
 
-    public:
-        void reshape(TensorShape shape);
+		/**
+		 * 获取元素大小
+		 * */
+		size_t getElementSize(DataType dtype) {
+			switch (dtype) {
+				case i8:
+				case u8:
+					return 1;
+				case i16:
+				case u16:
+				case f16:
+					return 2;
+				case i32:
+				case u32:
+				case f32:
+					return 4;
+				case i64:
+				case u64:
+				case f64:
+					return 8;
+				default:
+					assert(false);
+			}
+		}
 
-        Tensor asType(DataType data_type);
+		/**
+		 * 张量的形状
+		 * */
+		class TensorShape {
+		public:
+				TensorShape() : _dims({}) {}
 
-        Tensor slice();
+				TensorShape(const initializer_list<int> &list) : _dims(list) {}
 
-    public:
-        inline void *getData() const { return data.get(); }
+				inline int size() const {
+					int total = 1;
+					for (auto i:_dims)total *= i;
+					return total;
+				}
 
-        inline TensorShape getShape() const { return shape; }
+				inline int ndim() const { return _dims.size(); }
 
-        inline DataType getDataType() const { return data_type; }
+				inline int operator()(int index) const { return _dims[index]; }
 
-    protected:
-        shared_ptr<void> data;      // 允许多个张量共享同一块内存，例如：切片、广播
-        TensorShape shape;
-        DataType data_type;
-    };
+				int index(const TensorShape &index) const {
+					int ind = 0;
+					for (int i = 0; i < _dims.size(); i++) {
+						int step = index(i);
+						for (int j = i + 1; j < _dims.size(); j++) step *= _dims[j];
+						ind += step;
+					}
+
+					return ind;
+				}
+
+				string toString() const {
+					stringstream ss;
+
+					ss << "(";
+					if (_dims.size() > 0) ss << _dims[0];
+					for (int i = 1; i < _dims.size(); i++)ss << "," << _dims[i];
+					ss << ")";
+
+					return ss.str();
+				}
+
+		private:
+				vector<int> _dims;
+		};
+
+		ostream &operator<<(ostream &stream, const TensorShape &obj) {
+			return stream << obj.toString();
+		}
+
+		/**
+		 * 张量
+		 * */
+		class Tensor {
+		public:
+				Tensor(const TensorShape &shape, DataType dtype, string name)
+								: _shape(shape), _dtype(dtype), _name(move(name)) {
+					_data = make_shared<void>(malloc(shape.size() * getElementSize(dtype)), [](void *p) { free(p); });
+				}
+
+				Tensor(const TensorShape &shape, DataType dtype)
+								: _shape(shape), _dtype(dtype) {
+					_data = make_shared<void>(malloc(shape.size() * getElementSize(dtype)), [](void *p) { free(p); });
+				}
+
+		public:
+				void reshape(const TensorShape &shape) {
+					if (shape.size() != _shape.size()) {
+						stringstream ss;
+						ss << "ValueError: cannot reshape tensor of size " << _shape.size() << " into shape " << shape;
+						throw ValueError(ss.str());
+					}
+
+					_shape = shape;
+				}
+
+				//Tensor asType(DataType data_type);
+
+				template<typename T>
+				T at(const TensorShape &index) {
+					auto ind = _shape.index(index);
+					auto data = static_cast<T *> (_data.get());
+					return data[ind];
+				}
+
+				template<typename T>
+				T &at(const TensorShape &index) {
+					auto ind = _shape.index(index);
+					auto data = static_cast<T *> (_data.get());
+					return data[ind];
+				}
+
+		public:
+				template<typename T>
+				inline T *data() const { return (T *) _data.get(); }
+
+				inline const string &name() const { return _name; }
+
+				inline const TensorShape shape() const { return _shape; }
+
+				inline DataType dtype() const { return _dtype; }
+
+		private:
+				string _name;
+				TensorShape _shape;
+				shared_ptr<void> _data;      // 允许多个张量共享同一块内存，例如：切片、广播
+				DataType _dtype;
+		};
+
+		ostream &operator<<(ostream &stream, const Tensor &obj) {
+			if (obj.name().empty())
+				return stream << "Tensor" << obj.shape().toString();
+			else
+				return stream << "Tensor:" << obj.name() << obj.shape().toString();
+		}
 }
 
 #endif //TENSORSCRIPT_TENSOR_HPP
--- src/tensor.cpp
+++ src/tensor.cpp
@@ -3,7 +3,19 @@
 using namespace TensorScript;
 
 int main() {
-    Tensor tensor;
+	Tensor a({3, 4}, DataType::f32);
+	auto *ptr = a.data<float>();
+	for (int i = 0; i < 12; i++) fprintf(stdout, "%f ", ptr[i]);
+	fprintf(stdout, "\n");
 
-    return 0;
+	for (int i = 0; i < 12; i++) {
+		ptr[i] = (float) i * 2;
+	}
+
+//	for (int i = 0; i < 3; i++) {
+//		for (int j = 0; j < 4; j++) {
+//			fprintf(stdout, "%f ", a.at({i, j}));
+//		}
+//	}
+	return 0;
 }
