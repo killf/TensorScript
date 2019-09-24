a7b475124391354eb1ab4e3298b708cbe1701f9b
killf <killf@foxmail.com>
2019-09-24 10:12:12 +0800

    优化代码

--- include/tensor.hpp
+++ include/tensor.hpp
@@ -12,37 +12,33 @@ namespace TensorScript {
 		using namespace std;
 
 		/**
-		 * 值错误
-		 * */
-		class ValueError : public exception {
-		public:
-				explicit ValueError(const string &&msg) : _msg(msg) {}
-
-				const char *what() const noexcept override {
-					return _msg.c_str();
-				}
-
-		private:
-				string _msg;
-		};
-
-		/**
 		 * 自定义错误
 		 * */
 		class CustomError : public exception {
 		public:
+				explicit CustomError(string msg) : _msg(std::move(msg)) {}
+
 				explicit CustomError(const char *__restrict format, ...) {
+					static char buf[512];
+					int max_len = sizeof(buf);
+
 					va_list ap;
 					va_start(ap, format);
-
-					int max_len = strlen(format) + 4096;
-					auto buf = new char[max_len];
-					vsnprintf(buf, max_len, format, ap);
-
+					int len = vsnprintf(buf, max_len - 1, format, ap);
 					va_end(ap);
 
-					_msg = string(buf);
-					delete (buf);
+					if (len < max_len) {
+						_msg = string(buf);
+					} else {
+						auto str = new char[len + 2];
+
+						va_start(ap, format);
+						vsprintf(str, format, ap);
+						va_end(ap);
+
+						_msg = string(str);
+						delete[] str;
+					}
 				}
 
 				const char *what() const noexcept override {
@@ -58,19 +54,36 @@ namespace TensorScript {
 		 * */
 		class TensorShape {
 		public:
-				TensorShape() : _dims({}) {}
+				/**
+				 * 默认构造函数
+				 * */
+				TensorShape() = default;
 
+				/**
+				 * 构造TensorShape
+				 * */
 				TensorShape(const initializer_list<int> &list) : _dims(list) {
-					_size = 1;
 					for (auto i:_dims)_size *= i;
 				}
 
+				/**
+				 * 获取元素的个数，标量的size为1，但ndim为0
+				 * */
 				inline int size() const { return _size; }
 
+				/**
+				 * 获取维数
+				 * */
 				inline int ndim() const { return _dims.size(); }
 
-				inline int operator()(int index) const { return _dims[index]; }
+				/**
+				 * 访问指定的维度
+				 * */
+				inline int operator[](int index) const { return _dims[index]; }
 
+				/**
+				 * 根据下表计算索引
+				 * */
 				int index(const initializer_list<int> &index) const {
 					vector<int> index_v(index);
 					check_index(index_v);
@@ -85,14 +98,12 @@ namespace TensorScript {
 					return ind;
 				}
 
-				string toString() const {
+				/**
+				 * 格式化
+				 * */
+				inline string toString() const {
 					stringstream ss;
-
-					ss << "(";
-					if (_dims.size() > 0) ss << _dims[0];
-					for (int i = 1; i < _dims.size(); i++)ss << "," << _dims[i];
-					ss << ")";
-
+					ss << this;
 					return ss.str();
 				}
 
@@ -113,11 +124,14 @@ namespace TensorScript {
 
 		private:
 				vector<int> _dims;
-				int _size;
+				int _size = 1;
 		};
 
 		ostream &operator<<(ostream &stream, const TensorShape &obj) {
-			return stream << obj.toString();
+			stream << "(";
+			if (obj.ndim() > 0) stream << obj[0];
+			for (int i = 1; i < obj.ndim(); i++)stream << "," << obj[i];
+			stream << ")";
 		}
 
 		/**
@@ -126,35 +140,37 @@ namespace TensorScript {
 		template<typename T>
 		class Tensor {
 		public:
+				/**
+				 * 构造张量，并分配空间
+				 * */
 				explicit Tensor(const TensorShape &shape) : _shape(shape) {
 					_data.reset(new T[shape.size()]);
 				}
 
-				Tensor(string name, const TensorShape &shape) : _name(std::move(name)), _shape(shape) {
-					_data.reset(new T[shape.size()]);
+				/**
+				 * 构造张量，并使用指定的值进行初始化
+				 * */
+				Tensor(const TensorShape &shape, T value) : Tensor(shape) {
+					fill(value);
 				}
 
 		public:
+				/**
+				 * 调整张量的shape
+				 * */
 				void reshape(const TensorShape &shape) {
 					if (shape.size() != _shape.size()) {
 						stringstream ss;
 						ss << "ValueError: cannot reshape tensor of size " << _shape.size() << " into shape " << shape;
-						throw ValueError(ss.str());
+						throw CustomError(ss.str());
 					}
 
 					_shape = shape;
 				}
 
-				T operator()(const initializer_list<int> &index) const {
-					auto ind = _shape.index(index);
-					return data()[ind];
-				}
-
-				T &operator()(const initializer_list<int> &index) {
-					auto ind = _shape.index(index);
-					return (data()[ind]);
-				}
-
+				/**
+				 * 使用指定的值填充张量
+				 * */
 				void fill(T value) {
 					auto ptr = data();
 					for (auto i = 0; i < _shape.size(); i++, ptr++) {
@@ -162,11 +178,70 @@ namespace TensorScript {
 					}
 				}
 
+				/**
+				 * 使用1填充张量，等价于`fill(1)`
+				 * */
+				inline void ones() {
+					fill(static_cast<T>(1));
+				}
+
+				/**
+				 * 使用0填充张量，等价于`fill(0)`
+				 * */
+				inline void zeros() {
+					fill(static_cast<T>(0));
+				}
+
 		public:
+				/**
+				 * 访问张量中的元素
+				 * */
+				T operator[](const initializer_list<int> &index) const {
+					auto ind = _shape.index(index);
+					return data()[ind];
+				}
+
+				/**
+				 * 访问张量中的元素
+				 * */
+				T &operator[](const initializer_list<int> &index) {
+					auto ind = _shape.index(index);
+					return (data()[ind]);
+				}
+
+				/**
+				 * 访问张量中的元素
+				 * */
+				inline T operator[](int index) const { return this[{index}]; }
+
+				/**
+				 * 访问张量中的元素
+				 * */
+				inline T &operator[](int index) { return this[{index}]; }
+
+				/**
+				 * 格式化
+				 * */
+				inline string toString() const {
+					stringstream ss;
+					ss << this;
+					return ss.str();
+				}
+
+		public:
+				/**
+				 * 张量的名称，可选值
+				 * */
 				inline string name() const { return _name; }
 
+				/**
+				 * 张量的shape
+				 * */
 				inline TensorShape shape() const { return _shape; }
 
+				/**
+				 * 张量的地址
+				 * */
 				inline T *data() const { return _data.get(); }
 
 		private:
--- src/tensor.cpp
+++ src/tensor.cpp
@@ -4,8 +4,7 @@
 using namespace TensorScript;
 
 int main() {
-	Tensor<float> a({3, 4});
-	a.fill(10.f);
+	Tensor<float> a({3, 4}, 0);
 
 	auto *ptr = a.data();
 	for (int i = 0; i < 12; i++) printf("%f ", ptr[i]);
@@ -17,11 +16,11 @@ int main() {
 
 	for (int i = 0; i < 3; i++) {
 		for (int j = 0; j < 4; j++) {
-			a({i, j}) = i * 4 + j;
+			a[{i, j}] = i * 4 + j;
 		}
 	}
 
-	for (int i = 0; i < 12; i++) printf( "%f ", ptr[i]);
+	for (int i = 0; i < 12; i++) printf("%f ", ptr[i]);
 	printf("\n");
 	return 0;
 }
